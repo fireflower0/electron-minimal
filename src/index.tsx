ebdf00eb5589b9cbfa687c4642b10aa727e4fc2d
fireflower0 <fireflower0@hotmail.co.jp>
2021-05-29 16:31:52 +0900

    Fixed a src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect } from 'react';
+import { render } from 'react-dom';
+import styled from 'styled-components';
+
+const FormWrapper = styled.div`
+  margin: 10px;
+  padding: 10px;
+  background-color: #263238;
+`;
+
+const App = () => {
+  useEffect(() => {
+    document.title = 'My Electron App';
+    document.body.style.margin = '0';
+    document.body.style.backgroundColor = '#263238';
+  }, []);
+
+  return <FormWrapper>Hello, world!</FormWrapper>;
+};
 
 const container = document.getElementById('contents');
 
-ReactDOM.render(<p>Hello, world!</p>, container);
+render(<App />, container);
